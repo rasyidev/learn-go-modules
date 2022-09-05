5bda8175ae606cb1fd21daaebbe157487aa1db0a
rasyidev <habib.rasyid11@gmail.com>
2022-09-05 12:11:07 +0700

    new file:   aritmatika/main.go

---
+++ aritmatika/main.go
@@ -0,0 +1,14 @@
+package aritmatika
+
+func Jumlahkan(a, b float64) float64 {
+	return a + b
+}
+func Kalikan(a, b float64) float64 {
+	return a * b
+}
+func Bagi(a, b float64) float64 {
+	return a / b
+}
+func Kurangkan(a, b float64) float64 {
+	return a - b
+}
